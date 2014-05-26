fcd90b2bee7418be588237700eff71cfa1c33e58
Max Kueng <me@maxkueng.com>
2014-05-26 14:45:10 +0200

    trying...

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-var util = require('util'),
-	Readable = require('stream').Readable
-	http = require('http'),
-	parseXML = require('xml2js').parseString;
+var util = require('util');
+var Readable = require('stream').Readable;
+var http = require('http');
+var parseXML = require('xml2js').parseString;
+var moment = require('moment');
+var debug = require('debug')('somastation');
+
+var fs = require('fs');
 
 exports = module.exports = SomaStream;
 
@@ -48,11 +52,47 @@ function SomaStream (stationId, options) {
 	}
 
 	this.pollInterval = options.pollInterval || 60000;
+	this.targetedPollInterval = options.targetedPollInterval || 10000;
 	this.stationId = stationId;
 	this.currentTrack = null;
+
+	this.timer = null;
+
+	this.hitCount = 0;
+	this.missCount = 0;
+
+	this.targetedPollingMode = false;
 }
 
 util.inherits(SomaStream, Readable);
+
+SomaStream.prototype.targetedPolling = function (timeout) {
+	if (timeout < 0) { timeout = this.pollInterval; }
+	debug('target', timeout);
+	clearTimeout(this.timer);
+
+	this.targetedPollingMode = true;
+
+	setTimeout(function () {
+		this.loop();
+	}.bind(this), timeout);
+};
+
+SomaStream.prototype.normalPolling = function () {
+	debug('normal');
+	clearTimeout(this.timer);
+
+	this.targetedPollingMode = false;
+
+	setTimeout(function () {
+		this.loop();
+	}.bind(this), this.pollInterval);
+}
+
+SomaStream.prototype.pollTimeout = function () {
+	debug('targetedPollingMode', this.targetedPollingMode);
+	return (this.targetedPollingMode) ? this.targetedPollInterval : this.pollInterval;
+};
 
 SomaStream.prototype.checkNowPlaying = function (callback) {
 	var self = this;
@@ -61,8 +101,8 @@ SomaStream.prototype.checkNowPlaying = function (callback) {
 		if (err) { return callback(); } //self.emit('error', err); }
 
 		parseXML(xml, function (err, result) {
-			if (err) { return; }
-			if (!result.songs || !result.songs.song || !result.songs.song.length) { return; }
+			if (err) { return callback(); }
+			if (!result.songs || !result.songs.song || !result.songs.song.length) { return callback(); }
 
 			var song, time, artist, title, album, trackId;
 
@@ -78,27 +118,38 @@ SomaStream.prototype.checkNowPlaying = function (callback) {
 			if (self.currentTrack !== trackId) {
 				self.currentTrack = trackId;
 
+				self.hitCount += 1;
+				debug('hit', +moment.utc() - time);
+				self.normalPolling();
+
 				self.push({
 					time: time,
 					artist: artist,
 					title: title,
 					album: album
 				});
-			}
-		});
 
-		callback();
+			} else {
+				debug('miss');
+				self.missCount += 1;
+			}
+
+			debug('hitrate',  (self.hitCount / (self.hitCount + self.missCount) * 100) + '%' );
+			fs.appendFileSync('./hitrate.csv', moment.utc().format('YYYY-MM-DDTHH:mm:ss') + ';' + (Math.round( (self.hitCount / (self.hitCount + self.missCount) * 100) * 100) / 100) + '\n', 'utf8');
+
+			callback();
+		});
 	});
 };
 
 SomaStream.prototype.loop = function () {
-	var self = this;
+	this.checkNowPlaying(function () {
+		this.timer = setTimeout(function () {
+			this.loop();
 
-	self.checkNowPlaying(function () {
-		setTimeout(function () {
-			self.loop();
-		}, self.pollInterval);
-	});
+		}.bind(this), this.pollTimeout());
+
+	}.bind(this));
 };
 
 SomaStream.prototype.start = function () {
